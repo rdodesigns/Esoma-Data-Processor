b0c549897edf2975d3f6a14d5ecb837ea8426604
Ryan Orendorff <ryan@rdodesigns.com>
2012-01-21 16:30:41 -0500

    Removed dummy test PulseOx2.cs

--- src/main.cs
+++ src/main.cs
@@ -8,14 +8,11 @@ class DataProcessor
   {
     DataRecord.DataRecordGenerator drg = new DataRecord.DataRecordGenerator();
     Device.PulseOx po = new Device.PulseOx(drg);
-    Device.PulseOx2 po2 = new Device.PulseOx2(drg);
     po.start();
-    po2.start();
 
     while(Console.ReadKey(true).Key != ConsoleKey.Escape){ }
 
     po.stop();
-    po2.stop();
     //System.Environment.Exit(0);
   }
 
