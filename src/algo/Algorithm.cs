d1e43a7c6dba864044cd151dc614af7531dc0e4f
Ryan Orendorff <ryan@rdodesigns.com>
2012-02-05 21:33:17 +0000

    Fixed algorithm run with only required fields all updated.

--- src/algo/Algorithm.cs
+++ src/algo/Algorithm.cs
@@ -1,3 +1,23 @@
+/**
+ * @file
+ * @author Ryan Orendorff <ryan@rdodesigns.com>
+ *
+ * @section DESCRIPTION
+ *
+ * Abstract class for running algorithms on DataRecords.
+ *
+ * @section CONTACT
+ *
+ *  email: esoma@rdodesigns.com
+ *    www: http://www.rdodesigns.com
+ * github: https://github.com/rdodesigns
+ *
+ * @section LICENSE
+ *
+ * Copyright by Ryan Orendorff, 2012 under the New BSD License. See
+ * LICENSE.txt for more details.
+ */
+
 using System;
 using System.Collections;
 
@@ -5,18 +25,27 @@ namespace Algorithm
 {
   public abstract class Algorithm
   {
+    // Data to be added to DataRecord
     protected Hashtable data = new Hashtable();
+    // Fields required for the calculation, these are those collected from the
+    // Device classes.
     public string[] requiredDataFields;
 
+    // Register the data types to be added to the DataRecord after the
+    // algorithm runs. Use only "data.Add(string key, object initial_value)"
+    // to add data to the DataRecord object.
     protected abstract void registerDataTypes();
+
+    // Code to run against the incoming data record.
     protected abstract void run(DataRecord.DataRecord incoming);
 
+    // Process the DataRecord. This
     public void process(ref DataRecord.DataRecord incoming){
-      bool runme = false;
+      bool runme = true;
       try{
         foreach (string field in requiredDataFields){
-          if (incoming.updated_fields.Contains(field))
-              runme = true;
+          if (!incoming.updated_fields.Contains(field))
+              runme = false;
         }
         if (runme) this.run(incoming);
         this.run(incoming);
